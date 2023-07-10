962388ebc1535bb64067a3b4b93fc5d0f92bd175
Nguyễn Thị Lan Anh <anhntl@s-wifi.vn>
2023-07-10 19:16:31 +0700

    update all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,94 @@
 import { Component } from '@angular/core';
 
-import { ClassicEditor as ClassicEditorBase } from '@ckeditor/ckeditor5-editor-classic';
-import { Undo } from '@ckeditor/ckeditor5-undo';
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
 import { Bold, Code, Italic, Strikethrough, Underline, Subscript, Superscript } from '@ckeditor/ckeditor5-basic-styles';
 
+import { UploadAdapter } from '@ckeditor/ckeditor5-adapter-ckfinder';
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload } from '@ckeditor/ckeditor5-image';
+import { Link } from '@ckeditor/ckeditor5-link';
+import { List } from '@ckeditor/ckeditor5-list';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { SpecialCharacters, SpecialCharactersEssentials } from '@ckeditor/ckeditor5-special-characters';
+import { CKFinder } from '@ckeditor/ckeditor5-ckfinder';
+import { Alignment } from '@ckeditor/ckeditor5-alignment'; // <--- ADDED
+import { Font } from '@ckeditor/ckeditor5-font';
+// Plugins to include in the build.
+ClassicEditor.builtinPlugins = [
+    Essentials,
+    UploadAdapter,
+    Autoformat,
+    BlockQuote,
+    Bold, Italic, Code, Strikethrough, Underline, Subscript, Superscript,
+    Font,
+    Heading,
+    Image,
+    ImageCaption,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Link,
+    List,
+    Paragraph,
+    Alignment,
+    Table,
+    TableToolbar,
+    SpecialCharacters, SpecialCharactersEssentials,
+    CKFinder
+];
+
+// Editor configuration.
+ClassicEditor.defaultConfig = {
+    toolbar: {
+        items: [
+            'undo', 'redo',
+            '|',
+            'heading',
+            '|',
+            'alignment', 'bold', 'italic', 'underline',
+            '|',
+            'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor',
+            '|',
+            'bulletedList', 'numberedList',
+            'link', 'uploadImage',
+            'blockQuote',
+            'insertTable',
+            'specialCharacters',
+        ]
+    },
+    table: {
+        contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+    },
+    image: {
+        toolbar: [
+            'imageStyle:inline',
+            'imageStyle:block',
+            'imageStyle:side',
+            '|',
+            'toggleImageCaption',
+            'imageTextAlternative'
+        ]
+    },
+    ckfinder: {
+        // Upload the images to the server using the CKFinder QuickUpload command.
+        uploadUrl: 'https://example.com/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images&responseType=json'
+    },
+    // This value must be kept in sync with the language defined in webpack.config.js.
+    language: 'en'
+};
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-    public Editor = ClassicEditorBase;
+    public Editor = ClassicEditor;
 
-    public isDisabled = false;
-    public editorData =
-        `<p><a href="https://yandex.ru/">https://yandex.ru/</a></p>`;
-
-    public componentEvents: string[] = [];
-    public config = {
-        plugins: [Bold, Italic, Underline, Strikethrough, Code, Subscript, Superscript, Undo],
-        toolbar: {
-            items: ['undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough', 'code', 'subscript', 'superscript']
-        }
-    }
 
 }
