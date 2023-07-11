cd7500003afb0a6bbfaeeb279fb0c1a09a46d9ee
lananh <anhntl@vntt.com.vn>
2023-07-11 13:39:30 +0700

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-
-import ClassicEditor from 'ckeditor5-build-classic-plus'
+import { Component, VERSION } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
@@ -8,63 +7,66 @@ import ClassicEditor from 'ckeditor5-build-classic-plus'
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    name = 'Angular ' + VERSION.major;
+    mainMatForm!: FormGroup;
+    availablePlugins: any[] = [];
+    get ckeditorTextFormElement() { return this.mainMatForm.get('ckeditor_text'); }
+    ckEditorConfig: any = {
+        toolbar: [
+            ['Source', 'Templates', 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat'],
+            ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'],
+            ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl'],
+            ['Link', 'Unlink', 'Anchor'],
+            ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe'],
+            ['Styles', 'Format', 'Font', 'FontSize'],
+            ['TextColor', 'BGColor'],
+            ['Maximize', 'ShowBlocks']
+        ]
+    };
 
-    public Editor = ClassicEditor;
-    public config = {
-        toolbar: {
-            items: [
-                'undo',
-                'redo',
-                '|',
-                'heading',
-                'fontFamily',
-                'fontSize',
-                '|',
-                'bold',
-                'italic',
-                'underline',
-                'fontColor',
-                'fontBackgroundColor',
-                'highlight',
-                '|',
-                'link',
-                'CKFinder',
-                'imageUpload',
-                'mediaEmbed',
-                '|',
-                'alignment',
-                'bulletedList',
-                'numberedList',
-                '|',
-                'indent',
-                'outdent',
-                '|',
-                'insertTable',
-                'blockQuote',
-                'specialCharacters'
-            ],
-        },
-        image: {
-            toolbar: [
-                'imageStyle:full',
-                'imageStyle:side',
-                '|',
-                'imageTextAlternative'
-            ]
-        },
-        table: {
-            contentToolbar: [
-                'tableColumn',
-                'tableRow',
-                'mergeTableCells'
-            ]
-        },
+    constructor(private formBuilder: FormBuilder) { }
 
+    ngOnInit(): void {
+        this.setupMainMatForm();
     }
-    // public editorsReady(editor) {
-    //     editor.ui.getEditableElement().parentElement.insertBefore(
-    //         editor.ui.view.toolbar.element,
-    //         editor.ui.getEditableElement()
-    //     );
-    // }
+
+    setupMainMatForm(): boolean {
+        const result = false;
+        this.mainMatForm = this.createMainMatForm();
+        return result;
+    }
+    createMainMatForm(): FormGroup {
+        let result = null;
+        const formBuilderItem: any = {};
+
+        formBuilderItem['ckeditor_text'] = ['<p>Normal</p> <h1>Heading 1</h1> <h2>Heading 2</h2> <h3>Heading 3</h3> <h4>Heading 4</h4> <h5>Heading 5</h5> <h6>Heading 6</h6> <pre> Formatted</pre> <address>Address</address> <div>Normal div</div>'];
+
+        result = this.formBuilder.group(formBuilderItem);
+        return result;
+    }
+
+    onMainMatFormNgSubmit($event: any): boolean {
+        const result = true;
+
+        return result;
+    }
+
+
+
+    onCkEditorReady(editor: any): boolean {
+        const result = true;
+        // editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+        //   return new UploadAdapter (loader, {}, currentComponent.httpClient);
+        // };
+
+
+        // console.log('editor');
+        // console.log(editor);
+
+        return result;
+    }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { CKEditorModule } from 'ckeditor4-angular';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, CKEditorModule],
+    imports: [BrowserModule, ReactiveFormsModule, FormsModule, CKEditorModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
